1073cb6cec354c45ec014517e8b85ce45b5a6908
piusatwau <atwaupius7@gmail.com>
2026-03-15 00:33:37 +0300

    started db connection tutorial script

---
+++ data_from_databases.R
@@ -0,0 +1,12 @@
+# Data bases
+# R for data science tutorial
+# https://r4ds.hadley.nz/databases.html
+
+# loading necessary libraries for accessing databases
+# these packages help to connect to databases, send queries, and manipulate the 
+# results in R
+
+library(DBI)
+library(dbplyr)
+library(tidyverse)
+
