965bd952e15bd5c94c668e4699b57a7aa4d8b058
piusatwau <atwaupius7@gmail.com>
2026-03-24 10:11:05 +0300

    genomic ranges, creating genomic range, using promoters and flank functions, overview of karyoploteR package

--- bioinformatics/GenomicRanges/genomicranges.R
+++ bioinformatics/GenomicRanges/genomicranges.R
@@ -15,6 +15,7 @@ library(IRanges)
 library(GenomicFeatures)
 
 #-------------------------------------------------------------------------------
+# 1. IRanges
 
 # creating IRanges
 ir1 <- IRanges(c(1, 5, 10), c(3, 7, 15))
@@ -45,3 +46,36 @@ shift(ir2, shift = 4)
 # comparisons and overlaps
 ?findOverlaps
 findOverlaps(ir1, ir2)
+
+
+#===============================================================================
+# GenomicRanges
+
+library(BiocManager)
+BiocManager::install('karyoploteR')
+
+library(karyotype)# required to plot GRange objects
+
+# creating GRange object
+gr1 <- GRanges(seqnames = 'chr1', strand = c('+', '-', '+'), 
+               ranges = IRanges(start = c(1,3, 5), width = 3 ) )
+
+gr1
+
+plotGR(gr1)
+
+?GRanges
+
+flank(gr1) # function creates new ranges that are located to the side of the 
+# original ranges. It is commonly used to isolate upstream regulatory regions or downstream termination regions.
+promoters(gr1) # extract the promoter region around the Transcription Start Site (TSS)
+
+
+#-------------------------------------------------------------------------------
+# Example using karyoploteR
+# 1. Define the genomic context (e.g., human genome hg19)
+# We limit it to chr1 since that is where gr1 is located
+kp <- plotKaryotype(genome = "hg19", chromosomes = "chr1")
+
+# 2. Plot your gr1 object
+plotGR(kp, data = gr1)
